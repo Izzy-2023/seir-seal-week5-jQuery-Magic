4c89ede425ced807e59508462fc25b6e4891c127
izzy-2023 <izzyzinxhirija@gmail.com>
2023-12-03 12:11:10 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -12,9 +12,46 @@ $(() => {
     console.log($h1)
     // Append the h1 element to the container
     $container.append($h1);
-  
-
 });
 
+$(() => {
+    // Select the element with ID "container"
+    const $container = $("#container");
+	// Create an h2 element
+    const $h2 = $("<h2>Izzy</h2>");
+    // Append the h2 element to the container
+    $container.append($h2);
+});
+
+$(() => {
+    // Select the element with ID "container"
+    const $container = $("#container");
+    // Create an h3 element 
+    const $h3 = $("<h3>Ravenclaw</h3>")
+    // Append the h3 element to the container
+    $container.append($h3);
+})
+
+
+$(() => {
+    // Select the element with the ID container
+    const $container = $("#container");
+    // Create an h4 element
+    const $h4 = $("<h4>dragon</h4>");
+    // Add a class to the h4 element
+    $h4.addClass("cat")
+    // Append the h4 element to the container
+    $container.append($h4);
+})
+
+$(() => {
+    // Select the element with ID container
+    const $container = $("#container");
+    // Create h5 element
+    const $h5 = $("<h5>Hornbeam want with Dragon Heartstring</h5>");
+    // Append the h5 element to the container
+    $container.append($h5);
+
+})
       
 
